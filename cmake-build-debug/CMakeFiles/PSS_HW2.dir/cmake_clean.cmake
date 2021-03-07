ec43f2e99cb7e40963fe8100ab9da1148d87e3f4
Artem <artmurashko@yandex.ru>
2021-03-07 13:38:41 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PSS_HW2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSS_HW2.dir/main.cpp.o"
+  "PSS_HW2"
+  "PSS_HW2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSS_HW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
